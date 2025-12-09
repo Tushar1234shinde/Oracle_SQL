3b926b7b2ca09028b2e8d9bb916f26cad7c110df
Tushar1234shinde <100269635+Tushar1234shinde@users.noreply.github.com>
2025-12-09 14:04:47 +0530

    Add files via upload

--- Substitute_variable.sql
+++ Substitute_variable.sql
@@ -1,4 +1,4 @@
-
+select * from employees;
 
 -- substitute variable
 
@@ -11,3 +11,20 @@ select employee_id, last_name, job_id, &col_name
 from employees
 where &condition
 order by &col;
+
+
+select first_name, &&col_name,  &col1,&Com2 from employees
+where &&condition;
+
+
+define a=200;
+
+select * from &&table_name
+where employee_id=&a;
+
+undefine a;
+
+set verify on;
+
+set verify off;
+
