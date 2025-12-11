1138dd3de5712b755fdda94a1aac9b37edf7c54b
Tushar1234shinde <100269635+Tushar1234shinde@users.noreply.github.com>
2025-12-11 13:26:29 +0530

    Add files via upload

---
+++ conversion_function.sql
@@ -0,0 +1,12 @@
+-- Conversion Function
+
+--number to char
+select 'This is charachter ' || 10 from dual;
+
+select 'This is charachter ' || 20  as "sdfgsufisgfisdgvbsdjkvbjksdvb" from dual;
+
+select 'This is charachtesdfhsjkdvhjskhvr ' || 20  as "sdfgsufisgfisdgvbsdjkvbjksdvb" from dual;
+
+--char to number
+select '2' + 4 as "sgadsfgiuribgkjbvfdnvjk uvgiufhvkjnv" from dual;
+
--- date_functions.sql
+++ date_functions.sql
@@ -11,3 +11,51 @@ select sessiontimezone, current_timestamp from dual;
 select last_name, (sysdate-hire_date)/7 as "Week" from employees;
 
 
+select months_between('01-SEP-05','11-JAN-04') from dual;
+
+select months_between('02-FEB-07','27-APR-09') from dual;
+
+select add_months(sysdate,-7) from dual;
+
+select add_months(sysdate,2) from dual;
+
+select next_day(current_date,'Wednesday') from dual;
+
+select next_day(hire_date,'Sunday') as "Revised date" from employees
+order by "Revised date";
+
+select last_day(current_date) from dual;
+
+select last_day('12-Nov-25') from dual;
+
+select last_day('12-FEB-26') from dual;
+
+select last_day('21-Dec-26') from dual;
+
+
+select round(sysdate,'Month') from dual;
+
+select round(to_date('20-DEC-25'),'Month') from dual;
+
+select round(to_date('05-MAr-25'),'Month') from dual;
+
+select round(to_date('16-DEC-25'),'Month') from dual;
+
+select round(sysdate,'Year') from dual;
+
+select round(to_date('16-May-25'),'Year') from dual;
+
+select round(to_date('16-Mar-25'),'Year') from dual;
+
+select round(to_date('16-Jan-25'),'Year') from dual;
+
+select trunc(sysdate,'month') from dual;
+
+select trunc(sysdate,'year') from dual;
+
+select trunc(to_date('12-Nov-25'),'Year') from dual;
+
+
+
+
+
