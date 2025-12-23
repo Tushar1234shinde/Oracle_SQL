f37c0f714ed5b2289170796ef4834e14c1c61bdd
Tushar1234shinde <100269635+Tushar1234shinde@users.noreply.github.com>
2025-12-23 12:42:33 +0530

    Add files via upload

--- group_clause.sql
+++ group_clause.sql
@@ -13,8 +13,24 @@ from employees
 where salary>40
 group by department_id, job_id
 order by department_id,job_id desc;
+--having
+select department_id, avg(salary)
+from employees
+group by department_id
+having avg(salary)>8000;
+
+select department_id, max(salary)
+from employees
+group by department_id
+having max(salary)>10000;
+
+select job_id,sum(salary)payroll
+from employees
+where job_id not like '%rep%'
+group by job_id
+having sum(salary)<13000
+order by sum(salary);
 
 
-
-
-
+select count(avg(salary)) from employees
+group by department_id;
