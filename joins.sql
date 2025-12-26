2fec852e292f9a8f15a725f739a2f21724a4954b
Tushar1234shinde <100269635+Tushar1234shinde@users.noreply.github.com>
2025-12-26 13:14:51 +0530

    Add files via upload

--- joins.sql
+++ joins.sql
@@ -1,67 +1,168 @@
-select * from employees natural join jobs;
+--JOINS
+--1. natural join
+--2. using clause
+--3. on clause
+--4. self join
+--5. non-equi join
+--6. outer joins
+--    i. left outer join
+--    ii. right outer join
+--    iii. full outer join
+--7. cross join
 
-select * from employees natural join job_history;
 
-select * from job_history;
 
-select employee_id, first_name, job_id, job_title, department_id
-from employees natural join jobs;
+--natural join
+SELECT
+    *
+FROM
+         employees
+    NATURAL JOIN jobs;
 
-select employee_id, first_name, job_id, job_title, department_id
-from employees natural join jobs
-where department_id not in (20,50);
+SELECT
+    *
+FROM
+         employees
+    NATURAL JOIN job_history;
 
-select last_name,salary , department_name, location_id
-from employees join departments
-using(department_id);
+SELECT
+    *
+FROM
+    job_history;
 
-select employee_id, first_name, job_id, job_title, department_id
-from employees natural join jobs
-where department_id  in (20,50);
+SELECT
+    employee_id,
+    first_name,
+    job_id,
+    job_title,
+    department_id
+FROM
+         employees
+    NATURAL JOIN jobs;
 
-select * from employees
-where employee_id = (select employee_id from employees 
-where department_id=20 
-fetch first 1 row only)
-or employee_id =
-(select employee_id from employees 
-where department_id=50 
-fetch first 1 row only)
-order by department_id;
+SELECT
+    employee_id,
+    first_name,
+    job_id,
+    job_title,
+    department_id
+FROM
+         employees
+    NATURAL JOIN jobs
+WHERE
+    department_id IN ( 20, 50 );
 
-select e.employee_id, e.last_name, e.department_id,
-d.department_id, d.department_name
- from employees e join departments d
- on(e.department_id=d.department_id);
+SELECT
+    employee_id,
+    first_name,
+    job_id,
+    job_title,
+    department_id
+FROM
+         employees
+    NATURAL JOIN jobs
+WHERE
+    department_id NOT IN ( 20, 50 );
+
+--using clause
+SELECT
+    last_name,
+    salary,
+    department_name,
+    location_id
+FROM
+         employees
+    JOIN departments USING ( department_id );
+
+--subquery
+SELECT
+    *
+FROM
+    employees
+WHERE
+    employee_id = (
+        SELECT
+            employee_id
+        FROM
+            employees
+        WHERE
+            department_id = 20
+        FETCH FIRST 1 ROW ONLY
+    )
+    OR employee_id = (
+        SELECT
+            employee_id
+        FROM
+            employees
+        WHERE
+            department_id = 50
+        FETCH FIRST 1 ROW ONLY
+    )
+ORDER BY
+    department_id;
+
+--on clause
+SELECT
+    e.employee_id,
+    e.last_name,
+    e.department_id,
+    d.department_id,
+    d.department_name
+FROM
+         employees e
+    JOIN departments d ON ( e.department_id = d.department_id );
  
- select employee_id, department_name, city
- from employees e join departments d
- on(e.department_id=d.department_id)
- join locations l
- on(l.location_id=d.location_id);
+--three way join with on clause
+SELECT
+    employee_id,
+    department_name,
+    city
+FROM
+         employees e
+    JOIN departments d ON ( e.department_id = d.department_id )
+    JOIN locations   l ON ( l.location_id = d.location_id );
+
+--three way join with using clause
+SELECT
+    employee_id,
+    department_name,
+    city
+FROM
+         employees e
+    JOIN departments d USING ( department_id )
+    JOIN locations   l USING ( location_id );
+
+SELECT
+    e.employee_id,
+    e.last_name,
+    e.department_id,
+    d.department_id,
+    d.department_name
+FROM
+         employees e
+    JOIN departments d ON ( e.department_id = d.department_id )
+WHERE
+    e.manager_id = 149;
+
+SELECT
+    e.employee_id,
+    e.last_name,
+    e.department_id,
+    d.department_id,
+    d.department_name
+FROM
+         employees e
+    JOIN departments d ON ( e.department_id = d.department_id )
+                          AND e.manager_id = 149;
  
-  select employee_id, department_name, city
- from employees e join departments d
- using(department_id)
- join locations l
- using(location_id);
- 
- select e.employee_id, e.last_name, e.department_id,
-d.department_id, d.department_name
- from employees e join departments d
- on(e.department_id=d.department_id)
- where e.manager_id=149;
- 
- select e.employee_id, e.last_name, e.department_id,
-d.department_id, d.department_name
- from employees e join departments d
- on(e.department_id=d.department_id)
- and e.manager_id=149;
- 
- 
- select e.last_name emp, m.last_name mgr
- from employees e join employees m
- on(e.manager_id=m.employee_id)
- where e.last_name = 'Abel';
- 
- 
+-- self join
+SELECT
+    e.last_name emp,
+    m.last_name mgr
+FROM
+         employees e
+    JOIN employees m ON ( e.manager_id = m.employee_id )
+WHERE
+    e.last_name = 'Abel';
+    
+--non-equi join
