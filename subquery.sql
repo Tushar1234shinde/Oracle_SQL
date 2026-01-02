a5ed000071debc7fc060a85471a20cdfc1bda56d
Tushar1234shinde <100269635+Tushar1234shinde@users.noreply.github.com>
2026-01-02 15:58:45 +0530

    Add files via upload

--- subquery.sql
+++ subquery.sql
@@ -1,5 +1,7 @@
 --Subquery
 
+
+--single row subquery
 select hire_date from employees where last_name='Davies';
 
 select last_name, hire_date from employees where hire_date>'29-JAN-15';
@@ -48,3 +50,53 @@ select employee_id, last_name from employees where salary=2500;
 select last_name,job_id from employees
 where job_id=
 (select job_id from employees where last_name='Haas');
+
+
+--multi row subquery
+select salary from employees where job_id = 'IT_PROG';
+
+select employee_id, job_id, salary, last_name
+from employees
+where salary<any 
+(select salary from employees where job_id = 'IT_PROG')
+and job_id <> 'IT_PROG';
+
+select employee_id, job_id, salary, last_name
+from employees
+where salary<all 
+(select salary from employees where job_id = 'IT_PROG')
+and job_id <> 'IT_PROG';
+
+select employee_id, job_id, salary, last_name
+from employees
+where salary in 
+(select salary from employees where job_id = 'IT_PROG')
+and job_id <> 'IT_PROG';
+
+
+
+--multi column subquery
+select first_name, department_id, salary
+from employees
+where (salary, department_id) in
+(select min(salary), department_id
+from employees 
+group by department_id)
+order by department_id;
+
+--correlated subquery
+select last_name, salary, department_id
+from employees outer_table
+where salary>(select avg(salary) from employees inner_table
+where inner_table.department_id=outer_table.department_id)
+order by department_id;
+
+
+
+--exist operator
+
+select employee_id, last_name, department_id, job_id
+from employees outer
+where exists (select null
+from employees
+where manager_id=outer.employee_id);
